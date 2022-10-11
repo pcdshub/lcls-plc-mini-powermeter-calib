0c43fcf8a72eb9942a2b4a02ce6e4298af82749c
Zachary Lentz <zlentz@slac.stanford.edu>
2022-10-11 14:57:50 -0700

    BLD: build the IOC correctly this time

---
+++ iocBoot/ioc-tst-mini-powermeter-calib/st.cmd
@@ -0,0 +1,158 @@
+#!/cds/group/pcds/epics/ioc/common/ads-ioc/R0.6.0/bin/rhel7-x86_64/adsIoc
+################### AUTO-GENERATED DO NOT EDIT ###################
+#
+#         Project: lcls-plc-mini-powermeter-calib.tsproj
+#        PLC name: mini_powermeter_calib (mini_powermeter_calib Instance)
+# Generated using: pytmc 2.14.0
+# Project version: c873215
+#    Project hash: c8732153ef64f91e579b09d07a78439140807dc1
+#     PLC IP/host: 172.21.148.185
+#      PLC Net ID: 172.21.148.185.1.1
+#  ** Production mode IOC **
+#  Using /cds/data/iocData for autosave and archiver settings.
+#
+# Libraries:
+#
+#   LCLS General: * -> 2.6.0 (SLAC)
+#   lcls2-cc-lib: * -> 1.1.3 (SLAC)
+#   Tc2_Standard: * (Beckhoff Automation GmbH)
+#   Tc2_System: * (Beckhoff Automation GmbH)
+#   Tc3_Module: * (Beckhoff Automation GmbH)
+#
+################### AUTO-GENERATED DO NOT EDIT ###################
+< envPaths
+
+epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
+
+epicsEnvSet("ENGINEER", "zlentz" )
+epicsEnvSet("LOCATION", "PLC:TST:WFS:SPM:CALIB" )
+epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
+epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
+
+# Run common startup commands for linux soft IOC's
+< /reg/d/iocCommon/All/pre_linux.cmd
+
+# Register all support components
+dbLoadDatabase("$(ADS_IOC_TOP)/dbd/adsIoc.dbd")
+adsIoc_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet("ASYN_PORT",        "ASYN_PLC")
+epicsEnvSet("IPADDR",           "172.21.148.185")
+epicsEnvSet("AMSID",            "172.21.148.185.1.1")
+epicsEnvSet("AMS_PORT",         "851")
+epicsEnvSet("ADS_MAX_PARAMS",   "1051")
+epicsEnvSet("ADS_SAMPLE_MS",    "50")
+epicsEnvSet("ADS_MAX_DELAY_MS", "100")
+epicsEnvSet("ADS_TIMEOUT_MS",   "1000")
+epicsEnvSet("ADS_TIME_SOURCE",  "0")
+
+# Add a route to the PLC automatically:
+system("${ADS_IOC_TOP}/scripts/add_route.sh 172.21.148.185 ^172.*")
+
+# adsAsynPortDriverConfigure(portName, ipaddr, amsaddr, amsport,
+#    asynParamTableSize, priority, noAutoConnect, defaultSampleTimeMS,
+#    maxDelayTimeMS, adsTimeoutMS, defaultTimeSource)
+# portName            Asyn port name
+# ipAddr              IP address of PLC
+# amsaddr             AMS Address of PLC
+# amsport             Default AMS port in PLC (851 for first PLC)
+# paramTableSize      Maximum parameter/variable count. (1000)
+# priority            Asyn priority (0)
+# noAutoConnect       Enable auto connect (0=enabled)
+# defaultSampleTimeMS Default sample of variable (PLC ams router
+#                     checks if variable changed, if changed then add to send buffer) (50ms)
+# maxDelayTimeMS      Maximum delay before variable that has changed is sent to client
+#                     (Linux). The variable can also be sent sooner if the ams router
+#                     send buffer is filled (100ms)
+# adsTimeoutMS        Timeout for adslib commands (1000ms)
+# defaultTimeSource   Default time stamp source of changed variable (PLC=0):
+#                     PLC=0: The PLC time stamp from when the value was
+#                         changed is used and set as timestamp in the EPICS record
+#                         (if record TSE field is set to -2=enable asyn timestamp).
+#                         This is the preferred setting.
+#                     EPICS=1: The time stamp will be made when the updated data
+#                         arrives in the EPICS client.
+adsAsynPortDriverConfigure("$(ASYN_PORT)", "$(IPADDR)", "$(AMSID)", "$(AMS_PORT)", "$(ADS_MAX_PARAMS)", 0, 0, "$(ADS_SAMPLE_MS)", "$(ADS_MAX_DELAY_MS)", "$(ADS_TIMEOUT_MS)", "$(ADS_TIME_SOURCE)")
+
+cd "$(ADS_IOC_TOP)/db"
+
+
+dbLoadRecords("iocSoft.db", "IOC=PLC:TST:WFS:SPM:CALIB")
+dbLoadRecords("save_restoreStatus.db", "P=PLC:TST:WFS:SPM:CALIB:")
+dbLoadRecords("caPutLog.db", "IOC=$(IOC)")
+
+## TwinCAT task, application, and project information databases ##
+dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=$(ASYN_PORT),PREFIX=PLC:TST:WFS:SPM:CALIB,IDX=1")
+dbLoadRecords("TwinCAT_AppInfo.db", "PORT=$(ASYN_PORT), PREFIX=PLC:TST:WFS:SPM:CALIB")
+
+dbLoadRecords("TwinCAT_Project.db", "PREFIX=PLC:TST:WFS:SPM:CALIB,PROJECT=lcls-plc-mini-powermeter-calib.tsproj,HASH=c873215,VERSION=c873215,PYTMC=2.14.0,PLC_HOST=172.21.148.185")
+
+#   LCLS General: * -> 2.6.0 (SLAC)
+dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:TST:WFS:SPM:CALIB,DEPENDENCY=LCLS_General,VERSION=2.6.0,VENDOR=SLAC")
+#   lcls2-cc-lib: * -> 1.1.3 (SLAC)
+dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:TST:WFS:SPM:CALIB,DEPENDENCY=lcls2-cc-lib,VERSION=1.1.3,VENDOR=SLAC")
+#   Tc2_Standard: * (Beckhoff Automation GmbH)
+dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:TST:WFS:SPM:CALIB,DEPENDENCY=Tc2_Standard,VERSION=*,VENDOR=Beckhoff Automation GmbH")
+#   Tc2_System: * (Beckhoff Automation GmbH)
+dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:TST:WFS:SPM:CALIB,DEPENDENCY=Tc2_System,VERSION=*,VENDOR=Beckhoff Automation GmbH")
+#   Tc3_Module: * (Beckhoff Automation GmbH)
+dbLoadRecords("TwinCAT_Dependency.db", "PREFIX=PLC:TST:WFS:SPM:CALIB,DEPENDENCY=Tc3_Module,VERSION=*,VENDOR=Beckhoff Automation GmbH")
+
+cd "$(IOC_TOP)"
+
+## PLC Project Database files ##
+dbLoadRecords("mini_powermeter_calib.db", "PORT=$(ASYN_PORT),PREFIX=PLC:TST:WFS:SPM:CALIB:,IOCNAME=$(IOC),IOC=$(IOC)")
+
+# Total records: 51
+callbackSetQueueSize(2102)
+
+# Autosave and archive settings:
+save_restoreSet_status_prefix("PLC:TST:WFS:SPM:CALIB:")
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+set_pass0_restoreFile("info_positions.sav")
+set_pass1_restoreFile("info_settings.sav")
+
+# ** Production IOC Settings **
+set_savefile_path("$(IOC_DATA)/$(IOC)/autosave")
+set_requestfile_path("$(IOC_DATA)/$(IOC)/autosave")
+
+# Production IOC autosave files go in iocData:
+cd "$(IOC_DATA)/$(IOC)/autosave"
+
+# Create info_positions.req and info_settings.req
+makeAutosaveFiles()
+
+cd "$(IOC_DATA)/$(IOC)/archive"
+
+# Create $(IOC).archive
+makeArchiveFromDbInfo("$(IOC).archive", "archive")
+cd "$(IOC_TOP)"
+
+# Configure access security: this is required for caPutLog.
+asSetFilename("$(ACF_FILE)")
+
+# Initialize the IOC and start processing records
+iocInit()
+
+# Enable logging
+iocLogInit()
+
+# Configure and start the caPutLogger after iocInit
+epicsEnvSet(EPICS_AS_PUT_LOG_PV, "$(IOC):caPutLog:Last")
+
+# caPutLogInit("HOST:PORT", config)
+# config options:
+#       caPutLogNone       -1: no logging (disable)
+#       caPutLogOnChange    0: log only on value change
+#       caPutLogAll         1: log all puts
+#       caPutLogAllNoFilter 2: log all puts no filtering on same PV
+caPutLogInit("$(EPICS_CAPUTLOG_HOST):$(EPICS_CAPUTLOG_PORT)", 0)
+
+# Start autosave backups
+create_monitor_set( "info_positions.req", 10, "" )
+create_monitor_set( "info_settings.req", 60, "" )
+
+# All IOCs should dump some common info after initial startup.
+< /reg/d/iocCommon/All/post_linux.cmd
+
